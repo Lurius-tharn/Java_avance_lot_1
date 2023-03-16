49bcc0b2a3452468ed272f49004dd028bd1c40c8
Davenese <sajous.davenese@gmail.com>
2023-03-16 10:51:10 +0100

    core

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,11 @@
 module com.esiee.java_avance_lot_1 {
     requires javafx.controls;
     requires javafx.fxml;
-            
-        requires org.controlsfx.controls;
-            requires com.dlsc.formsfx;
-            requires validatorfx;
-                requires org.kordamp.bootstrapfx.core;
-            
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
     opens com.esiee.java_avance_lot_1 to javafx.fxml;
     exports com.esiee.java_avance_lot_1;
 }
