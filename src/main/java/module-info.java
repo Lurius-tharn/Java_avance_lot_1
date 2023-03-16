207759b6041dcbcaea573a03c5cf2ef7cb6a29df
Davenese <sajous.davenese@gmail.com>
2023-03-16 11:19:59 +0100

    Ajout des dépendances XML

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.esiee.java_avance_lot_1 {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.xml.bind;
 
     opens com.esiee.java_avance_lot_1 to javafx.fxml;
     exports com.esiee.java_avance_lot_1;
