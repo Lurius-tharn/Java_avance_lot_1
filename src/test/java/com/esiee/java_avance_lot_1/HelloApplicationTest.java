d68c2390fa6dd9463d554585424f931c1a938737
Davenese <sajous.davenese@gmail.com>
2023-06-15 11:20:35 +0200

    ajout Tests

--- src/test/java/com/esiee/java_avance_lot_1/HelloApplicationTest.java
+++ src/test/java/com/esiee/java_avance_lot_1/HelloApplicationTest.java
@@ -3,13 +3,15 @@ package com.esiee.java_avance_lot_1;
 import com.esiee.java_avance_lot_1.vue.HomeApplication;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxAssert;
-import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 import org.testfx.matcher.control.LabeledMatchers;
 
 import java.io.IOException;
 
+@ExtendWith(ApplicationExtension.class)
 public class HelloApplicationTest {
 
     @Start
@@ -20,7 +22,7 @@ public class HelloApplicationTest {
     }
 
     @Test
-    public void should_click(FxRobot fxRobot) {
+    public void should_click() {
         FxAssert.verifyThat("#validerButton", LabeledMatchers.hasText("Valider"));
     }
 }
