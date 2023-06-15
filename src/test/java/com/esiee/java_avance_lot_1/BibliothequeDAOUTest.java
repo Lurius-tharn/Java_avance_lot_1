140c9c63576a70977d616103ed6d77411793cc00
Davenese <sajous.davenese@gmail.com>
2023-06-15 16:31:43 +0200

    ajout lombok et mockito

--- src/test/java/com/esiee/java_avance_lot_1/BibliothequeDAOUTest.java
+++ src/test/java/com/esiee/java_avance_lot_1/BibliothequeDAOUTest.java
@@ -3,19 +3,35 @@ package com.esiee.java_avance_lot_1;
 
 import com.esiee.java_avance_lot_1.dao.BibliothequeDao;
 import com.esiee.java_avance_lot_1.model.Bibliotheque;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BibliothequeDAOUTest {
     @InjectMocks
-    BibliothequeDao bibliothequeDaoMock;
+    BibliothequeDao bibliothequeDaoMock = new BibliothequeDao();
+
+    @BeforeAll
+    void setUp() throws SQLException, IOException {
+//        Properties prop = new Properties();
+//        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+//        InputStream stream = loader.getResourceAsStream("../../../config.properties");
+//        prop.load(stream);
+        BibliothequeDao.setDatabaseUrl("jdbc:h2:mem:ja_library;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+        BibliothequeDao.createDatabaseTable();
+
+    }
+
 
     @Test
     @DisplayName("devrait inserer une liste de livres")
@@ -32,6 +48,7 @@ public class BibliothequeDAOUTest {
                         .build())
                 .build());
 
+        bibliothequeDaoMock.insertBook(livres);
 
     }
 }
